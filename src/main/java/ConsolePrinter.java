bbcde409d59946cfcbb5bd29be48cebd9f863876
MaximM27 <makarovma27@gmail.com>
2022-03-23 15:48:15 +0300

    docs: К классу ConsolePrinter добавлен javadoc-комментарий

--- src/main/java/ConsolePrinter.java
+++ src/main/java/ConsolePrinter.java
@@ -1,4 +1,15 @@
+/**
+ * Класс ConsolePrinter необходим для описания методов
+ * по выводу в консоли сообщений различного содержания
+ * @author ma.makarov
+ */
 public class ConsolePrinter {
+    /**
+     * Метод print используется для вывода в консоли
+     * параметра message типа String
+     * @param message объект типа String, который требуется
+     * вывести в консоли
+     */
     public static void print(String message) {
         System.out.println(message);
     }

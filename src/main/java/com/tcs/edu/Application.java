21fa2fbea1085e51a12bfa848b32e4d94ce14697
MaximM27 <makarovma27@gmail.com>
2022-04-24 00:49:00 +0300

    feature: добавлен перечислимый тип Doubling. В метод print добавлена проверка на повторение выводимого сообщения

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -1,15 +1,16 @@
 package com.tcs.edu;
+import com.tcs.edu.decorator.Doubling;
 import com.tcs.edu.decorator.MessageOrder;
 import com.tcs.edu.decorator.Severity;
 import static com.tcs.edu.decorator.MessageService.print;
 
 class Application {
     public static void main(String[] args) {
-        print(Severity.MINOR, MessageOrder.ASC, "Hello world!", "Hello!", "Hi!", null, "Bye");
-        print(Severity.REGULAR, MessageOrder.DESC, "Hello world!", "Hello!", "Hi!", null, "Bye");
-        print(Severity.REGULAR,"Hello world!");
-        print(Severity.MAJOR,"Hello world!");
-        print(Severity.MINOR,"Hello world!");
-        print(Severity.MAJOR,"Hello world!");
+        //print(Severity.MINOR, MessageOrder.ASC, Doubling.DISTINCT, "Hello world!", "Hello!", "Bye","Hi!", null, "Bye");
+        //print(Severity.REGULAR, MessageOrder.DESC, Doubling.DOUBLES, "Hello world!", "Hello!", "Hello", "Hi!", null, "Bye");
+        print(Severity.REGULAR,MessageOrder.DESC, Doubling.DISTINCT, "Hello world!", "Hello", "Hello", "Hello","Hi", "Hi", null, "Bye");
+        //print(Severity.MAJOR,"Hello world!");
+        //print(Severity.MINOR,"Hello world!");
+        //print(Severity.MAJOR,"Hello world!");
     }
 }
--- src/main/java/com/tcs/edu/decorator/MessageService.java
+++ src/main/java/com/tcs/edu/decorator/MessageService.java
@@ -2,6 +2,8 @@ package com.tcs.edu.decorator;
 
 import com.tcs.edu.printer.ConsolePrinter;
 
+import java.util.Objects;
+
 import static com.tcs.edu.decorator.PrefixDecorator.messageCount;
 import static com.tcs.edu.decorator.SeparateMessageDecorator.separatePage;
 import static com.tcs.edu.decorator.SeverityDecorator.getMessageBySeverity;
@@ -46,28 +48,74 @@ public class MessageService {
      * метод используется для вывода
      * @param level типа Severity, определяющий уровень важности
      * @param order типа MessageOrder, определяющий порядок сортировки
+     * @param doubling типа Doubling, определяющий характер дублирования сообщений
      * @param message message объект типа String, который требуется декорировать
      * @param messages messages массив объектов типа String, которые требуется декорировать
      */
-    public static void print (Severity level, MessageOrder order, String message, String ... messages) {
+    public static void print (Severity level, MessageOrder order, Doubling doubling, String message, String ... messages) {
         ConsolePrinter.print(process(level, message));
-        switch (order) {
-            case ASC: {
-                for (String current: messages) {
-                    if (current != null) {
-                        ConsolePrinter.print(process(level, current));
+        switch (doubling) {
+            case DOUBLES: {
+                switch (order) {
+                    case ASC: {
+                        for (String current : messages) {
+                            if (current != null) {
+                                ConsolePrinter.print(process(level, current));
+                            }
                         }
+                        break;
                     }
+                    case DESC: {
+                        for (int counter = messages.length - 1; counter >= 0; counter--) {
+                            if (messages[counter] != null) {
+                                ConsolePrinter.print(process(level, messages[counter]));
+                            }
+                        }
+                        break;
+                    }
+                }
                 break;
             }
-            case DESC: {
-                for (int counter = messages.length - 1; counter > 0; counter--) {
-                    if (messages[counter] != null) {
-                        ConsolePrinter.print(process(level, messages[counter]));
+            case DISTINCT: {
+                String[] array = new String[messages.length];
+                switch (order) {
+                    case ASC: {
+                        for (int counter = 0; counter < messages.length; counter++) {
+                            if (messages[counter] != null) {
+                                int matchCounter = getMatchCounter(array, messages, counter);
+                                if (matchCounter == 0) {
+                                    ConsolePrinter.print(process(level, messages[counter]));
+                                    array[counter] = messages[counter];
+                                }
+                            }
                         }
+                        break;
                     }
+                    case DESC: {
+                        for (int counter = messages.length - 1; counter >= 0; counter--) {
+                            if (messages[counter] != null) {
+                                int matchCounter = getMatchCounter(array, messages, counter);
+                                if (matchCounter == 0) {
+                                    ConsolePrinter.print(process(level, messages[counter]));
+                                    array[counter] = messages[counter];
+                                }
+                            }
+                        }
+                        break;
+                    }
+                }
                 break;
             }
         }
+    }
+
+    private static int getMatchCounter(String[] array, String[] messages, int counter) {
+        int matchCounter = 0;
+        for (int arrayCounter = 0; arrayCounter < array.length; arrayCounter++) {
+            if (Objects.equals(array[arrayCounter], messages[counter])) {
+                matchCounter++;
+            }
+        }
+        return matchCounter;
     }
 }

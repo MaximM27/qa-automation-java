31b5ee76e42daaa18741198e52b231984b938792
MaximM27 <makarovma27@gmail.com>
2022-04-23 04:15:39 +0300

    feature: добавлен метод print в класс MessageService с параметром order типа MessageOrder для определения порядка сортировки выведенных сообщений

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -1,11 +1,12 @@
 package com.tcs.edu;
+import com.tcs.edu.decorator.MessageOrder;
 import com.tcs.edu.decorator.Severity;
 import static com.tcs.edu.decorator.MessageService.print;
 
 class Application {
     public static void main(String[] args) {
-        print(Severity.MINOR, "Hello world!", "Hello!", null, "Hi!");
-        print(Severity.REGULAR,"Hello world!");
+        print(Severity.MINOR, MessageOrder.ASC, "Hello world!", "Hello!", "Hi!", null, "Bye");
+        print(Severity.REGULAR, MessageOrder.DESC, "Hello world!", "Hello!", "Hi!", null, "Bye");
         print(Severity.REGULAR,"Hello world!");
         print(Severity.MAJOR,"Hello world!");
         print(Severity.MINOR,"Hello world!");
--- src/main/java/com/tcs/edu/decorator/MessageService.java
+++ src/main/java/com/tcs/edu/decorator/MessageService.java
@@ -41,4 +41,33 @@ public class MessageService {
             }
         }
     }
+
+    /**
+     * метод используется для вывода
+     * @param level типа Severity, определяющий уровень важности
+     * @param order типа MessageOrder, определяющий порядок сортировки
+     * @param message message объект типа String, который требуется декорировать
+     * @param messages messages массив объектов типа String, которые требуется декорировать
+     */
+    public static void print (Severity level, MessageOrder order, String message, String ... messages) {
+        ConsolePrinter.print(process(level, message));
+        switch (order) {
+            case ASC: {
+                for (String current: messages) {
+                    if (current != null) {
+                        ConsolePrinter.print(process(level, current));
+                        }
+                    }
+                break;
+            }
+            case DESC: {
+                for (int counter = messages.length - 1; counter > 0; counter--) {
+                    if (messages[counter] != null) {
+                        ConsolePrinter.print(process(level, messages[counter]));
+                        }
+                    }
+                break;
+            }
+        }
+    }
 }

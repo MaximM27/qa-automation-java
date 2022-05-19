8ded16bd897756d6221c20ee1c6c892f5ec88aa5
MaximM27 <makarovma27@gmail.com>
2022-05-19 09:13:06 +0300

    fix: добавлено описание класса и метода

--- src/main/java/com/tinkoff/edu/decorator/ValidatedService.java
+++ src/main/java/com/tinkoff/edu/decorator/ValidatedService.java
@@ -2,7 +2,16 @@ package com.tinkoff.edu.decorator;
 
 import com.tinkoff.edu.domain.Message;
 
+/**
+ *Базовый абстрактный класс описывает методы валидацции входных параметров
+ * @author ma.makarov
+ */
 public abstract class ValidatedService {
+    /**
+     * метод описывает правила валидации входных параметров объектов класса Message
+     * @param message объект класса Message
+     * @return true или false
+     */
     public boolean isArgsValid(Message message){
         if (message.getBody() == null) return false;
         if (message.getBody().isEmpty()) return false;
